9f85451eb29e42b82c9d6bc2dfb6d165b56f4075
Irish <irishmjn@gmail.com>
2024-01-04 11:47:16 +0545

    git@github.com:Erishprajapati/jacvascript.git

---
+++ datatype.js
@@ -0,0 +1,15 @@
+//we can write alert browser
+/*console.log(3+3); 
+
+
+ let name="eris";
+ let age= 18
+ let relationship = null;
+ let  isloggedIn=false
+console.log()
+//null
+standalone value 
+//undefined
+*/
+
+
