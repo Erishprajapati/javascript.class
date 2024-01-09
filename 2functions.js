e25955aa00f30f22d24940880adfcb12bfa77ca9
Irish <irishmjn@gmail.com>
2024-01-09 18:02:42 +0545

    https://github.com/Erishprajapati/javascript.class.git

---
+++ 2functions.js
@@ -0,0 +1,37 @@
+//functions with objects and array 
+//just for shopping cart
+/*function calculateCartPrice(...num1){
+ return num1
+}
+//console.log(calculateCartPrice(200,400,600,2000,900,9000))
+const shoe ={
+    shoename:"NIKE",
+    price: "Rs 7000"
+}
+function handleObject(anyobject){
+    console.log(`shoename is ${anyobject.shoename} and price is ${anyobject.price}`);
+
+}
+handleObject(shoe);
+
+const dress ={
+    dressname: "Long white coat",
+    price: "Rs 5500"
+}
+function handleObject(anyobject){
+    console.log(`dressname is ${anyobject.dressname} and price is ${anyobject.price}`)
+}
+handleObject(dress);
+*/
+
+const scooter ={
+    scootername: "Aprilla SR 125",
+    prices: "Rs 3,90,000(Three lakh ninty thousand)"
+}
+function handleObject(anyobject){
+    console.log(`scootername is ${anyobject.scootername} and price is ${anyobject.price}`);
+}
+handleObject({
+ scootername: "Aprilla SR 120",
+ price:"rs 50000"
+})
