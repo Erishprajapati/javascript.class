75f32605dbf6391f648f39a6bdf0f7cb4aefbd90
Irish <irishmjn@gmail.com>
2024-02-01 22:29:02 +0545

    https://github.com/Erishprajapati/javascript.class.git

---
+++ exam.js
@@ -0,0 +1,9 @@
+var a = document.querySelector("h1")
+a.innerHTML = "Semester 4th is going on"
+a.style.color ="purple"
+a.style.backgroundColor = "white"
+a.style.border="3px solid black"
+a.style.textAlign="center"
+a.addEventListener("click",function(){
+   console.log("Terminal exams are happenning soon")
+})
