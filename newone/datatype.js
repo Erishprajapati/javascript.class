288ae117c738f6fe90076f46ebed8a02247a61fe
Erishprajapati <irishmjn@gmail.com>
2024-03-27 21:42:07 +0545

    datatypes

---
+++ newone/datatype.js
@@ -0,0 +1,22 @@
+ "use strict"; //treat all JS code as new version
+ //alert( 3 + 3) can use window.alert and we are using node.js not browser
+ 
+ //console.log(9 + 8);
+ //console.log("erish")
+
+let name = "erish";
+let age = 18
+let isLoggedIn = false
+
+/*datatypes
+number 
+bigint = used for large set of number
+string = " double cotation"
+boolean = true or false
+null = stand alone value
+undefined = not giving value
+symbol = used for uniqueness
+
+object
+*/
+console.log(typeof null)
---
+++ newone/script.js
@@ -0,0 +1,18 @@
+const accountId = 144545
+let accountEmail = "irishmjn@gmail.com";
+var accountPassword = "erish";
+accountCity = "lokanthali";
+
+ accountId = 2
+ accountEmail = "erishmjn@gmail.com";
+ accountPassword = "ganthye";
+ accountCity = "bhaktapur";
+ console.log(accountCity);
+
+console.log(accountPassword);
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table(accountId, accountPassword, accountCity);
