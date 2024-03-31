9bc12b318407d692941709da003918ee32f0d5e2
Erishprajapati <irishmjn@gmail.com>
2024-03-31 12:44:52 +0545

    operations

---
+++ newone/operation.js
@@ -0,0 +1,50 @@
+let value = '10'
+
+//console.log(typeof value);
+//console.log(value);
+
+//const firstName = "Erish";
+//const lastName = "prajapati";
+
+//let fullName = firstName + lastName;
+//console.log(fullName);
+
+/* the information of someone 
+const firstName = "ayush";
+const lastName = "bajimaya";
+let number = 1234567;
+let location = "sankhamul";
+const gender = "male";
+let education = "bachelor";
+
+// Creating an object using the variables
+let information = {
+  firstName: firstName,
+  lastName: lastName,
+  gender: gender,
+  education: education,
+  number: number
+};
+
+console.log(information);
+
+
+const firstName = "bishal";
+const lastName = "prajapati";
+const gender = "male";
+const location = "lokanthali";
+let number = 1234590;
+let education = "bachelor";
+
+let information = {
+    firstName: firstName,
+    lastName: lastName,
+    gender: gender,
+    location: location,
+    education: education,
+    number: number
+}
+console.log(information);
+*/
+
+//adding string and number 
