ad402d12d9d508c39cf6801abfb53cd4c7db9956
Erishprajapati <irishmjn@gmail.com>
2024-03-31 08:09:29 +0545

    datatype conversion

---
+++ newone/conversion.js
@@ -0,0 +1,41 @@
+/*let age = "19abc"
+console.log(typeof age)
+converting the value of string to number of age 
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+ 
+let score = false 
+console.log(score)
+console.log(typeof score)
+
+let isLoggedIn = 1 
+console.log(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let isLoggedIn = ""
+console.log(isLoggedIn)
+let userLoggedIn = Boolean(isLoggedIn)
+console.log(userLoggedIn)
+
+let isLoggedIn = 0
+let userLoggedIn = Boolean(isLoggedIn)
+console.log(userLoggedIn)
+
+let isLoggedIn = " "
+let sumanisloggedIn = Boolean(isLoggedIn)
+console.log(sumanisloggedIn) 
+
+let isLoggedIn = 1;
+let erisLoggedIn = Boolean(isLoggedIn);
+console.log(erisLoggedIn); */
+
+let isLoggedIn = 0
+let userLoggedIn = Boolean(isLoggedIn)
+
+if(userLoggedIn) {
+    console.log("user has logged in");
+}else{
+    console.log("user has not logged in");
+}
