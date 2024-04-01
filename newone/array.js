da429de418a18ca188109ff9cf0b4a2659034dcf
Erishprajapati <irishmjn@gmail.com>
2024-04-01 22:41:09 +0545

    array

--- newone/array.js
+++ newone/array.js
@@ -6,4 +6,83 @@ let suman = "suman";
 let rabindra = "rabindra";
 const myArray = [erish, suman, rabindra]
 console.log(myArray[1])
+
+
+const myArray = [0, 1, 2, 4, 90, 59, 66, 74, 82, 94, 10];
+console.log(myArray[8])
+
+
+const erish ="erish";
+const suman ="suman";
+const heera = "heera";
+const myArray =["erish", "suman", "heera"];
+console.log(myArray[2]);
+
+
+let erish = "facebook";
+let sarasa = "instagram";
+let manish = "tiktok";
+let suyog = "pinterest";
+let samir = "youtube";
+
+let myArray = ["facebook", "instagram", "tiktok", "pinterest", "youtube"];
+//console.log(myArray.length);
+console.log(myArray[8]);
+
+
+const myArray1 = ["bhaktapur", "lalitpur", "kathamandu", "pokhara", "bagmati"];
+const myArray2 = ["Chanakya", "DAV", "Thames", "Shree", "Omega"];
+
+
+let myArray3 = myArray1 + myArray2;
+console.log(myArray1[0] +myArray2[0])
+//console.log(myArray3);
+
 */
+let name = ["erish", "suman", "rabindra", "saras", "samyam", "salin", "rahita"];
+let number = [98989898, 89898998, 232343445, 5656543, 34454433, 45423455, 234435555];
+let location = ["lokanthali", "hattiban", "pulchowk", "bhaisipati", "godawari", "mangalbazar"];
+let college = ["dav","united","LA","DAV", "Janapremi", "Pinnacle", "omega"];
+
+let information = [name, location, number, college]
+//console.log(name[1], location[1], number[1], college[4]);
+
+
+//array methods 
+information.push(2)
+console.log(information);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
