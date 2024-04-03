cdf57afee831142f888589e827045cbb6a8a4e0c
Erishprajapati <irishmjn@gmail.com>
2024-04-03 21:21:17 +0545

    document object model

--- DocumentOM/dom.js
+++ DocumentOM/dom.js
@@ -17,3 +17,28 @@ b.style.background="red"
 b.style.marginLeft = "10px"
 
 
+var c = document.querySelector("h5")
+c.innerHTML="there should be rule against abusing in futsal"
+document.querySelector("h5")
+c.style.background = "green"
+c.style.textAlign ="center"
+c.style.margin = "3 px solid black"
+c.style.fontSize = "100px"
+c.addEventListener("click", function(){
+    c.innerHTML = "but the authority has have been satisfied"
+    c.style.background = "blue"
+
+})
+
+a.addEventListener("click", function(){
+    a.innerHTML = "all of four has been destroyed"
+    a.style.background = "green"
+    a.style.textAlign = "center"
+    a.style.fontFamily ="Helvetica"
+})
+
+var project = document.querySelector("#project")
+var btn = document.querySelector("Click")
+btn.addEventListener("click", function(){
+    project.style.background = "blue"
+});
