2a785ea67b939b31e2d8bd04bc5fdf2cf22bad3c
Erishprajapati <irishmjn@gmail.com>
2024-04-03 20:52:41 +0545

    document object model

---
+++ DocumentOM/dom.js
@@ -0,0 +1,19 @@
+//selection of a html element
+var a = document.querySelector("h1")
+//console.log(a)
+var b = document.querySelector("h2")
+
+//changing html 
+a.innerHTML = "shreyians coding"
+b.innerHTML = "this a test"
+document.querySelector("h1").innerHTML = "Pillar of document object model"
+
+//changing css
+var a = document.querySelector("h1")
+a.style.color = "red"
+
+b.style.color = "blue"
+b.style.background="red"
+b.style.marginLeft = "10px"
+
+
