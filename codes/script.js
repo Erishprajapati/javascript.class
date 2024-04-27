fa0eba26164776fdee9ccd13eaa3be3bc099d40b
scrumbler <irishmjn@gmail.com>
2024-04-27 11:44:08 +0545

    object

--- codes/script.js
+++ codes/script.js
@@ -1,18 +1,48 @@
-let session = 25;
-let section = 5;
-let title = "javascript";
-
-const course = { 
-session : 25,
-section : 5,
-title : "javascript",
-notes:{
-    introduction : "Namaste from here" 
-},
-enroll(){
-    console.log("you are successfully enrolled");
-}
+//object literals 
+/*const fbUser = {
+    "fullName" : "Erish",
+    age: 19,
+    location: "lokanthali",
+    email: "irishmjn@gmail.com",
+    isLoggedIn: true,
+    lastLoginDays: ["sunday", "friday"]
 }
 
-course.enroll();
-console.log("course");
+console.log(fbUser.fullName)
+
+
+const accountHolder ={
+    fullName: "Bibek shah",
+    age: 35,
+    accountId: "1234567",
+    contactNumber: 987654567,
+    location: "sunakothi,lalitpur",
+    fatherName: "dhirendra shah",
+    motherName: "shanti devkota",
+}
+
+console.log(accountHolder)
+
+
+
+const studentInformation = {
+     fullName: "Erish Prajapati",
+     Semester: "fourth",
+     Number: +9779826765649,
+     location: "Lokanthali Bhaktapur",
+     fatherName: "Raj prajapati",
+     motherName: "Shova maharjan",
+     isPresent: false,
+     lastPresentDay: ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday"],
+}
+
+console.log(studentInformation["lastPresentDay"]);
+
+
+let peron = new Object();
+person.name = "sakar";
+person.email = "sakarbudhathoki@google.com";
+person.phone = "909988988988";
+person.isFootballer = "true";
+console.log(person.name);
+**/
