304b938b465b0afd232551b8bfd7a932ee72894d
scrumbler <irishmjn@gmail.com>
2024-04-27 11:21:09 +0545

    objects

---
+++ codes/script.js
@@ -0,0 +1,18 @@
+let session = 25;
+let section = 5;
+let title = "javascript";
+
+const course = { 
+session : 25,
+section : 5,
+title : "javascript",
+notes:{
+    introduction : "Namaste from here" 
+},
+enroll(){
+    console.log("you are successfully enrolled");
+}
+}
+
+course.enroll();
+console.log("course");
