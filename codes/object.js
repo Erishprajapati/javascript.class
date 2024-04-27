92ab94f8e402005763d568d2ae899954e605965b
scrumbler <irishmjn@gmail.com>
2024-04-27 12:47:32 +0545

    //singleton and object literals

---
+++ codes/object.js
@@ -0,0 +1,135 @@
+//object literals 
+/*const fbUser = {
+    "fullName" : "Erish",
+    age: 19,
+    location: "lokanthali",
+    email: "irishmjn@gmail.com",
+    isLoggedIn: true,
+    lastLoginDays: ["sunday", "friday"]
+}
+
+console.log(fbUser.fullName)
+
+
+const accountHolder ={
+    fullName: "Bibek shah",
+    age: 35,
+    accountId: "1234567",
+    contactNumber: 987654567,
+    location: "sunakothi,lalitpur",
+    fatherName: "dhirendra shah",
+    motherName: "shanti devkota",
+}
+
+console.log(accountHolder)
+
+
+
+const studentInformation = {
+     fullName: "Erish Prajapati",
+     Semester: "fourth",
+     Number: +9779826765649,
+     location: "Lokanthali Bhaktapur",
+     fatherName: "Raj prajapati",
+     motherName: "Shova maharjan",
+     isPresent: false,
+     lastPresentDay: ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday"],
+}
+
+console.log(studentInformation["lastPresentDay"]);
+
+
+let peron = new Object();
+person.name = "sakar";
+person.email = "sakarbudhathoki@google.com";
+person.phone = "909988988988";
+person.isFootballer = "true";
+console.log(person.name);
+
+//singleton
+
+const facebookUser = new Object();
+facebookUser.name = "Simanta kasaju",
+facebookUser.email= "simanta@gmail.com",
+facebookUser.password = "simantaishero",
+facebookUser.isLoggedIn = false,
+
+//console.log(facebookUser)
+
+const regularUser = {
+    email: "simantaishero@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"erish",
+            lastname:"prajapati",
+        }
+    }
+}
+
+//console.log(regularUser.fullname.userfullname.firstname);
+const obj1 = {
+1: "a",
+2: "b",
+3: "c",
+}
+
+const obj2={
+    4: "d",
+    5: "e",
+    6: "f",
+}
+
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3);
+
+//practicing with object keywords 
+const youtuberName = {
+    username:{
+        fullname:{
+            firstname: "aashutosh barahi",
+            age: 27,
+            location: "patan",
+            status: "not married",
+        }
+    }
+}
+console.log(youtuberName);
+
+const postContent = {
+    erish: "bajrang ban is very awesome",
+    sajat: "image2.png",
+    anish: "we won a team trophy in basketball tournament",
+    prithivi: "had a good lunch with her"
+}
+const comment = {
+    satwik: "wow! its very good erish",
+    suman: " the hillstation is awesome",
+    saugat: "congratulations anish",
+    samyam: " you too look awesome"
+}
+
+//const allOver= Object.assign({}, postContent, comment);
+//console.log(allOver);
+
+console.log("Post Content:");
+for (let key in postContent) {
+    console.log(`${key}: ${postContent[key]}`);
+}
+
+// Displaying the comments
+console.log("\nComment:");
+for (let key in comment) {
+    console.log(`${key}: ${comment[key]}`);
+}
+
+const users = [
+    {
+        id: 1, 
+        email: "erish@gmail.com"
+    }
+]
+
+users[1].email
+console
+**/
+
