ca06456c29a8bb4d3fbcba8f4877d6f05e2ea034
scrumbler <irishmjn@gmail.com>
2024-04-27 21:22:41 +0545

    narrow parts of functions

--- function /function.js
+++ function /function.js
@@ -1,4 +1,4 @@
-
+/** 
 function ourFunction(msg){
     console.log("welcome to our company");
     console.log("we are hiring frontend developers");
@@ -11,3 +11,34 @@ function myCollege(){
 
 ourFunction();
 myCollege();
+
+function myFunction(msg){
+console.log(msg);
+}
+myFunction("Apna college");
+
+function sum(a, b){
+console.log(a+b);
+}
+
+sum(9 + 9)
+
+
+//functiom declaration 
+function greet(student){
+    console.log("Namaste, " + student + "Swagat cha hajur lai");
+}
+
+//greet("erish")
+
+// how we can greet someone at our website 
+function say(name) {
+    function tell() {
+        console.log("Hello, " + name + "! Saw you after a long time.");
+    }
+    tell(); // Call the nested function
+}
+
+say("erish");
+
+**/
