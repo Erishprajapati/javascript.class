1348ca8e7421034d5be809f986dfe3121152b085
scrumbler <irishmjn@gmail.com>
2024-04-28 10:29:30 +0545

    forloops.js

---
+++ loops/forloops.js
@@ -0,0 +1,14 @@
+/**onsole.log('program started here')
+debugger
+for(let i = 0; i < 5; i++) {
+    console.log(i + 1);
+}
+
+const myFriends = ['erish', 'jenson', 'saugath', 'saras', 'mahesh', 'subidha']
+for(i = 0; i < 6; i++) {
+    console.log(`${i+1}.${myFriends[i]}`);
+    myFriends[i] = myFriends[i] + `thapa`;
+}
+
+**/
+//printing even numbers 
