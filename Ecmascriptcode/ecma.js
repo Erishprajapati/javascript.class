54ef7ddaf006b60f740c8fb3cba2abed9b6d740e
scrumbler <irishmjn@gmail.com>
2024-04-28 11:42:21 +0545

    arrow functions

---
+++ Ecmascriptcode/ecma.js
@@ -0,0 +1,65 @@
+// arrow function 
+/*
+function sayHi() {
+console.log("Hi");
+}
+sayHi();
+
+
+const sayHi = () => {
+    console.log("Hello");
+}
+const add =(a,b) => {
+    return a + b;
+}
+console.log(add(2,3));
+
+//sayHi();
+
+
+function students(){ // this is how we call normal functions 
+    console.log("erish ," + `welcoome to our college`);
+}
+students();
+
+const youtuber = () =>{ // this is how we call arrow functions 
+    console.log("piyush, " + `you have a visitor on your page`);
+}
+youtuber();
+
+
+// arguements in arrow functions 
+
+
+const location = () =>{
+console.log("lokanthali" + `is my homeplace`);
+}
+location();
+
+const home = ` is the place where i belong`;
+const location = () =>{
+    console.log("lokanthali" + home);
+    console.log("sunakothi" + `is my birthplce`);
+}
+location();
+
+// making entry form of an student 
+const name = "suman";
+const lastName = "thapa";
+const permanentlocation = "kapilbastu";
+let temporaryLocation = "hattiban";
+const gender = "male";
+let number = 908978878;
+const student = () =>{
+    console.log(name + lastName + ` lives in ` + permanentlocation + ` but nowadays he has shifted on` + temporaryLocation + ` his contact number is ` + number)
+}
+student();
+*/
+// basic arrow functions in js 
+function allow(name){
+    return `Namaste, ${name}`;
+}
+// arrow function 
+const allowfunction = (name) => `welcome, ${name}`;
+console.log(allow('Sahil'));
+console.log(allowfunction('Raman'));
---
+++ aynchronous/asynchronous.js
@@ -0,0 +1,5 @@
+const func1 = () =>{
+    console.log(`func1 is starting`)
+    func2()
+    console.log(`func1 us ended`)
+}
