f3aee93bf693ea2706b64811728f7b8cadc75ac3
scrumbler <irishmjn@gmail.com>
2024-05-01 20:54:46 +0545

    new changes in old document

--- randoms/script.js
+++ randoms/script.js
@@ -21,5 +21,24 @@ const car = {
 console.log(car)
 car.brand = 'Honda'
 console.log(car)
+
+//for loop
+for(i = 0; i<7; i++) {
+    console.log(i)
+}
+
+//while loops
+let i = 0;
+while(i < 5){
+    console.log(i)
+    i++;
+}
 */
 
+// array in javascript
+
+
+const button = document.querySelector('button');
+button.addEventListener('click', function(){
+    console.log('button clicked');
+})
