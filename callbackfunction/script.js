5052465f952dc25be7822d2f30fc8bfa3cf2e138
scrumbler <irishmjn@gmail.com>
2024-05-04 21:42:48 +0545

    arrow function with parameters

---
+++ callbackfunction/script.js
@@ -0,0 +1,16 @@
+//left side contains function expression 
+//right side contains arrow function concept
+const greet = (count)=>{
+    for(let i=0; i<count; i++)
+    console.log("namaste");
+}
+//greet(7);
+
+//with parameters
+const myFunction = (param1, param2) =>{
+    //function body 
+    console.log(param1,param2);
+}
+
+//calling the function 
+myFunction("namaste", "erish");
