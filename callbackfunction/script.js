cdb220aab48e1414bd1ba2b3af31457629ca8e62
scrumbler <irishmjn@gmail.com>
2024-05-04 22:01:51 +0545

    callback function

--- callbackfunction/script.js
+++ callbackfunction/script.js
@@ -1,3 +1,4 @@
+/** 
 //left side contains function expression 
 //right side contains arrow function concept
 const greet = (count)=>{
@@ -13,4 +14,67 @@ const myFunction = (param1, param2) =>{
 }
 
 //calling the function 
-myFunction("namaste", "erish");
+//myFunction("namaste", "erish");
+
+//callback function
+function welcome(name, callback) {
+    console.log("Hi" + " " + name);
+    callback()
+}
+//callback function 
+function callMe(){
+    console.log(" I am callback function");
+}
+//passing the function as arguements 
+//welcome("erish", callMe)
+
+function totalBill(name, callback) {
+    console.log("Hello" + " " + name);
+    callback()
+}
+
+function callMe(){
+    console.log(" your total amount is 18000");
+}
+//totalBill("sahil", callMe)
+
+
+function location(name, callback) {
+    console.log("Welcome " + name);
+    callback(); // Call the callback function
+}
+
+// Callback function
+function callMe() {
+    console.log("Your location is Bhaktapur");
+}
+
+// Passing the function as arguments
+location("Vianet", callMe);
+
+
+const calculate = (a, b, operation) =>{
+    return operation(a, b);
+}
+
+const result = calculate(8 , 2, function(num1, num2){
+    return num1 + num2;
+})
+console.log(result)
+
+//performing certain operations with some examples 
+const bill = (a, b, c, d, operation) => {
+    return operation(a, b, c, d);
+}
+
+const total = bill(100, 200, 250, 450, function(num1, num2, num3, num4) {
+    return num1 + num2 + num3 + num4;
+});
+
+console.log(total);
+**/
+
+
+
+
+
