1e30ac35fd5a3455868a4c1f3fc8926fd5d1baeb
scrumbler <irishmjn@gmail.com>
2024-06-21 22:53:04 +0545

    functions

---
+++ aynchronous/promises1.js
@@ -0,0 +1,9 @@
+function sayMyName(){
+    console.log("E");
+    console.log("R");
+    console.log("I");
+    console.log("S");
+    console.log("H");
+}
+
+sayMyName();
---
+++ oops/oop.js
@@ -0,0 +1,7 @@
+let baseSalary = 20_000;
+let overTime = 10;
+let rate = 30;
+
+function getIncome(baseSalary, overTime, rate) {
+    return baseSalary + (overTime * rate);
+}
