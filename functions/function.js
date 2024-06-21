fc4fdbc95b3133f7beaea2be763f2cf16e89ee12
scrumbler <irishmjn@gmail.com>
2024-06-21 14:46:34 +0545

    Initial commit

---
+++ functions/function.js
@@ -0,0 +1,16 @@
+function sayMyName(){
+    console.log("erish");
+}
+//sayMyName();
+
+function personInformation() {
+    return {
+        name: "Erish",
+        age: 20,
+        address: "Lokanthali"
+    };
+}
+
+const person = personInformation();
+//console.log(person);
+git
